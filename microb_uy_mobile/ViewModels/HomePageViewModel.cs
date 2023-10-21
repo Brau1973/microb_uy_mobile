f328bdc640ca91d80c795a2d2a1e4f733e62da5c
brauliojaureguy <brauliojaureguy12@gmail.com>
2023-10-21 12:25:39 -0300

    scroll en home

--- microb_uy_mobile/ViewModels/HomePageViewModel.cs
+++ microb_uy_mobile/ViewModels/HomePageViewModel.cs
@@ -24,22 +24,18 @@ namespace microb_uy_mobile
             // Cuando el servicio esté disponible, reemplaza el código de prueba con datos reales.
 
             // Ejemplo de datos hardcodeados (reemplaza esto con la integración real):
-            Posts = new ObservableCollection<PostDTO>
+            Posts = new ObservableCollection<PostDTO>();
+
+            for (int i = 1; i <= 20; i++)
             {
-                new PostDTO
+                Posts.Add(new PostDTO
                 {
-                    UserProfileImage = "profile_image1.jpg",
-                    UserName = "Usuario 1",
-                    PostContent = "Este es el contenido del primer post."
-                },
-                new PostDTO
-                {
-                    UserProfileImage = "profile_image2.jpg",
-                    UserName = "Usuario 2",
-                    PostContent = "Este es el contenido del segundo post."
-                },
-                // Agrega más datos de prueba según sea necesario.
-            };
+                    UserProfileImage = "diego_forlan.jpg",
+                    UserName = $"Usuario {i}",
+                    PostContent = $"Este es el contenido del post {i}."
+                });
+            }
+
         }
     }
 }
