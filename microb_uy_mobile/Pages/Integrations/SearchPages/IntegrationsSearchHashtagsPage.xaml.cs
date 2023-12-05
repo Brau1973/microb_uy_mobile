a3a07c99957f4e5937e9e9ca9e141a8529fb0cd1
brauliojaureguy <brauliojaureguy12@gmail.com>
2023-12-04 23:01:40 -0300

    undo base home page etc

--- microb_uy_mobile/Pages/Integrations/SearchPages/IntegrationsSearchHashtagsPage.xaml.cs
+++ microb_uy_mobile/Pages/Integrations/SearchPages/IntegrationsSearchHashtagsPage.xaml.cs
@@ -1,24 +1,11 @@
-using microb_uy_mobile.Pages.BasePages;
-
 namespace microb_uy_mobile.Pages.Integrations.SearchPages;
 
-public partial class IntegrationsSearchHashtagsPage : BaseSearchHashtagsPage
+public partial class IntegrationsSearchHashtagsPage : ContentPage
 {
 	public IntegrationsSearchHashtagsPage()
 	{
 		InitializeComponent();
         // Ocultar completamente la barra de navegación
         NavigationPage.SetHasNavigationBar(this, false);
-    }
-
-    public override void OnSearchButtonClicked(object sender, EventArgs e)
-    {
-        string searchText = base.CloseKeyboardAndGetSearchText();
-        DisplayAlert("Integrations instancia Search HASHTAGS SearchBar", "Buscando " + searchText, "OK");
-
-        // Aquí puedes agregar la lógica de búsqueda con el texto ingresado
-
-        // Por ejemplo, podrías actualizar la CollectionView con nuevos resultados de búsqueda
-        // searchResultsCollectionView.ItemsSource = PerformSearch(searchText);
     }
 }
